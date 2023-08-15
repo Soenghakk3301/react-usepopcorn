5779075ee83d12cc69adc215970d2e5c89ba5cb0
soenghakk3301 <soenghakkchoeurn3301@gmail.com>
2023-08-15 14:11:41 +0700

    thinking of state and reusability

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import './index.css'
+// import App from './App'
+import StarRating from './StarRating'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <StarRating maxStarRating={10} />
+    <StarRating />
+    <StarRating messages={['terrible', 'bad', 'okay', 'good', 'amazing']} />
+    <StarRating defaultRating={3} />
+  </React.StrictMode>
+)
